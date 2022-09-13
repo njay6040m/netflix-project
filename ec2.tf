207543a1493352e39172e723fa431e692fc469da
njay.chidubem <njay.njay6040@gmail.com>
2022-09-13 12:14:10 +0100

    adding code to create ec2 instance

---
+++ ec2.tf
@@ -0,0 +1,21 @@
+# code to create ec2
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.30.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
